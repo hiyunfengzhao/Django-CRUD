76177d7ffb31e2c4d311f7a0c11dd4d6a9519ca4
hiyunfengzhao <27982597+hiyunfengzhao@users.noreply.github.com>
2019-07-13 00:07:39 -0700

    Django CRUD

---
+++ mysite/login/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from login import models
+# Create your views here.
+
+user_list = []
+
+def index(request):
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('username')
+        models.UserInfo.objects.create(user=username, pwd=password)
+    user_list = models.UserInfo.objects.all()
+    return render(request, 'index.html',{'data':user_list})
